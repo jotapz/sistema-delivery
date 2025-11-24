cc7c1dfc659c5c3c6079a02c458c63cc5d7e0d3c
João Pedro <82528746+jotapz@users.noreply.github.com>
2025-11-24 19:58:49 -0300

    adicionando ON DELETE CASCADE

--- delivery.sql
+++ delivery.sql
@@ -1,43 +1,41 @@
 CREATE DATABASE delivery;
-
 USE delivery;
 
-SELECT*FROM Restaurante;
-SELECT*FROM Cliente;
-SELECT*FROM Pedido;
-SELECT*FROM ItemPedido;
-
-
-
 CREATE TABLE Cliente(
-cliente_id INT PRIMARY KEY AUTO_INCREMENT,
-nome VARCHAR(100) NOT NULL,
-telefone VARCHAR(100) NOT NULL,
-endereco VARCHAR(100)
+    cliente_id INT PRIMARY KEY AUTO_INCREMENT,
+    nome VARCHAR(100) NOT NULL,
+    telefone VARCHAR(100) NOT NULL,
+    endereco VARCHAR(100)
 );
 
 CREATE TABLE Restaurante(
-restaurante_id INT PRIMARY KEY AUTO_INCREMENT,
-nome VARCHAR(100) NOT NULL,
-tipo_cozinha VARCHAR(100),
-telefone VARCHAR(100)
+    restaurante_id INT PRIMARY KEY AUTO_INCREMENT,
+    nome VARCHAR(100) NOT NULL,
+    tipo_cozinha VARCHAR(100),
+    telefone VARCHAR(100)
 );
 
 CREATE TABLE Pedido (
-pedido_id INT PRIMARY KEY AUTO_INCREMENT,
-cliente_id INT,
-restaurante_id INT,
-data_hora DATETIME,
-status_pedido VARCHAR(100),
-FOREIGN KEY (cliente_id) REFERENCES cliente(cliente_id),
-FOREIGN KEY (restaurante_id) REFERENCES restaurante(restaurante_id)
+    pedido_id INT PRIMARY KEY AUTO_INCREMENT,
+    cliente_id INT,
+    restaurante_id INT,
+    data_hora DATETIME,
+    status_pedido VARCHAR(100),
+    FOREIGN KEY (cliente_id) REFERENCES Cliente(cliente_id) ON DELETE CASCADE,
+    FOREIGN KEY (restaurante_id) REFERENCES Restaurante(restaurante_id) ON DELETE CASCADE
 );
 
 CREATE TABLE ItemPedido(
-item_id INT PRIMARY KEY AUTO_INCREMENT,
-pedido_id INT,
-descricao VARCHAR(100),
-quantidade INT,
-preco DECIMAL(10, 2),
-FOREIGN KEY (pedido_id) REFERENCES pedido(pedido_id)
+    item_id INT PRIMARY KEY AUTO_INCREMENT,
+    pedido_id INT,
+    descricao VARCHAR(100),
+    quantidade INT,
+    preco DECIMAL(10, 2),
+    FOREIGN KEY (pedido_id) REFERENCES Pedido(pedido_id) ON DELETE CASCADE
 );
+
+
+SELECT*FROM Cliente;
+SELECT*FROM Restaurante;
+SELECT*FROM Pedido;
+SELECT*FROM ItemPedido;
